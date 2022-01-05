f3ab34f28f86a996353047158fe54f77d88fac8b
Primexz <johann@pepebot.info>
2022-01-05 23:59:50 +0100

    add: version to package.json, add most fields to stats embed

--- commands/other/other_statistic.js
+++ commands/other/other_statistic.js
@@ -4,7 +4,8 @@ const utils = require("../../modules/utils");
 const discordClient = require("../../handlers/VariableHandler").client;
 const Permissions = Discord.Permissions.FLAGS
 const os = require('os')
-
+const { version } = require('../../package.json');
+const discordJs = require('../../package.json').dependencies["discord.js"]
 
 module.exports = {
 
@@ -14,7 +15,7 @@ module.exports = {
     async execute(client, interaction) {
 
         const ping = client.ws.ping
-        const streams = `${client.distube.listenerCount()} / ${client.distube.getMaxListeners()}`
+        const voiceStreams = `${client.distube.listenerCount()} / ${client.distube.getMaxListeners()}`
         const shards = client.options.shardCount
         const guilds = client.guilds.cache.size
         const userCount = client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)
@@ -25,14 +26,29 @@ module.exports = {
         const totalMemory = os.totalmem() / (1024 * 1024 * 1024)
         const memoryPercent = `${((os.freemem() / os.totalmem()) * 100).toFixed(2)}%`
 
+        const time1 = new Date()
+        await interaction.reply("Collecting information..")
+        const time2 = new Date()
 
-        interaction.reply({
+        const responseTime = time2-time1
+
+        await interaction.editReply({
+            content: null,
             embeds: [new Discord.MessageEmbed()
                 .setColor(utils.EmbedColors.Default)
                 .setTitle(`FireFly Statistics`)
+                .addField("Version", `\`\`\`FireFly:    ${version}\nDiscord.js: ${discordJs}\`\`\``, true)
                 .addField("CPU Info", `\`\`\`${cpuCount} cores\n${cpuModel}\`\`\``, true)
                 .addField("Memory Usage", `\`\`\`${usedMemory.toFixed(2)} / ${totalMemory.toFixed(2)} GB\n${memoryPercent}\`\`\``, true)
                 .addField("Uptime", `\`\`\`${uptime}\`\`\``)
+                .addField("Websocket Ping", `\`\`\`${ping}ms\`\`\``, true)
+                .addField("Response Ping", `\`\`\`${responseTime}ms\`\`\``, true)
+                .addField("Voice Streams", `\`\`\`${voiceStreams}\`\`\``, true)
+
+
+                .addField("User", `\`\`\`${userCount}\`\`\``, true)
+                .addField("Server", `\`\`\`${guilds}\`\`\``, true)
+                .addField("Shards", `\`\`\`${shards}\`\`\``, true)
 
                 .setFooter({
                     text: utils.Embeds.footerText,
