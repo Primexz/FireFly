37d23ad375a09653577dcefbd1da95208ddbdd94
Primexz <johann@pepebot.info>
2022-01-12 21:02:44 +0100

    rename: command_avatar.js to other_avatar.js

--- commands/other/other_avatar.js
+++ commands/other/other_avatar.js
@@ -6,45 +6,12 @@ const Permissions = Discord.Permissions.FLAGS
 module.exports = {
 
 
-    name: 'avatar',
+    name: 'help',
     permissions: [],
 
     async execute(client, interaction) {
 
-        let user;
-        try {
-            user = await interaction.guild.members.fetch(interaction.options.getUser("user"))
-        }
-        catch {
-            return interaction.reply({
-                embeds: [new Discord.MessageEmbed()
-                    .setColor(utils.EmbedColors.Error)
-                    .setTitle(`${utils.Icons.error} User not found`)
-                    .setDescription("This user don't exist on your guild!")
-                    .setFooter({
-                        text: utils.Embeds.footerText,
-                        iconURL: discordClient.user.displayAvatarURL({dynamic: true})
-                    })
-                    .setTimestamp(new Date())]
-            })
-        }
 
-        let avatar = user.user.displayAvatarURL({ dynamic: true });
-        if (!avatar.endsWith('?size=2048')) avatar += "?size=2048";
-
-        interaction.reply({
-            embeds: [new Discord.MessageEmbed()
-                .setColor(utils.EmbedColors.Default)
-                .setAuthor(user.user.username, user.user.displayAvatarURL({ dynamic: true }))
-                .addField("URL", `[Avatar URL](${user.user.displayAvatarURL({ dynamic: true })})`)
-                .setTitle(`👤 Avatar`)
-                .setImage(avatar)
-                .setFooter({
-                    text: utils.Embeds.footerText,
-                    iconURL: discordClient.user.displayAvatarURL({dynamic: true})
-                })
-                .setTimestamp(new Date())]
-        })
 
     },
 };
