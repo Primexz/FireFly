ea38e097d98c8bf3c023e38a604a282f3944194c
Primexz <johann@pepebot.info>
2022-01-12 21:36:08 +0100

    add: embed reply to "help" command

--- commands/other/other_help.js
+++ commands/other/other_help.js
@@ -12,12 +12,25 @@ module.exports = {
 
     async execute(client, interaction) {
 
-        await interaction.editReply({
+        await interaction.reply({
             content: null,
             embeds: [new Discord.MessageEmbed()
                 .setColor(utils.EmbedColors.Default)
                 .setTitle(`${utils.Icons.fire} FireFly - Help`)
-                .setDescription(``)
+                .setDescription(`
+        Here is a list of all important commands for FireFly.
+        If you need details about a command, just enter the command in the chat to get more information.
+
+        :musical_note: **__music__**
+        > \`\`play\`\`, \`\`manager\`\`, \`\`filter\`\`, \`\`nowplaying\`\`, \`\`volume\`\`
+        
+        :bulb: **__other__**
+        > \`\`help\`\`, \`\`avatar\`\`, \`\`stats\`\`
+        
+
+        __**Useful links**__
+        [Homepage](https://xyz.de/) | [Invite Pepe](https://discord.gg) | [Support Server](https://discord.gg) | [Vote](https://top.gg)
+                `)
                 .setFooter({
                     text: utils.Embeds.footerText,
                     iconURL: client.user.displayAvatarURL({dynamic: true})
