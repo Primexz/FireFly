6ba61a57fc78ed04dbf4d4865e49722bd8a9c782
Primexz <johann@pepebot.info>
2022-01-19 23:02:37 +0100

    fix: discordActivityHandler.js fetch now every shard

--- handlers/discordActivityHandler.js
+++ handlers/discordActivityHandler.js
@@ -15,10 +15,15 @@ module.exports = async (client) => {
 
     async function nextStatus() {
         let status = Stats[Math.floor(Math.random() * Stats.length)];
+
+        const guildCount = (await client.shard.fetchClientValues('guilds.cache.size')).reduce((acc, guildCount) => acc + guildCount, 0)
+        const userCount = (await client.shard.broadcastEval(c => c.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0))).reduce((acc, memberCount) => acc + memberCount, 0);
+        const voiceStreams = (await client.shard.fetchClientValues('voice.adapters.size')).reduce((acc, voiceCount) => acc + voiceCount, 0);
+
         const formattedStatus = status.text
-            .replace(/{guildCount}/g, utils.formatInt(client.guilds.cache.size))
-            .replace(/{streamCount}/g, utils.formatInt(client.voice.adapters.size))
-            .replace(/{userCount}/g, utils.formatInt(client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)));
+            .replace(/{guildCount}/g, utils.formatInt(guildCount))
+            .replace(/{streamCount}/g, utils.formatInt(voiceStreams))
+            .replace(/{userCount}/g, utils.formatInt(userCount));
         client.user.setActivity(formattedStatus, {type: status.type});
     }
 
