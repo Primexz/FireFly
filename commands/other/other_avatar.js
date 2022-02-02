3dae85e25066781ee3c2600fd9a4017e39b39e1a
Primexz <johann@pepebot.info>
2022-02-02 22:16:11 +0100

    fix: broken avatar command

--- commands/other/other_avatar.js
+++ commands/other/other_avatar.js
@@ -6,12 +6,50 @@ const Permissions = Discord.Permissions.FLAGS
 module.exports = {
 
 
-    name: 'help',
+    name: 'avatar',
     permissions: [],
 
     async execute(client, interaction) {
 
+        let user;
+        try {
+            user = await interaction.guild.members.fetch(interaction.options.getUser('user'))
+        }
+        catch {
+            return await interaction.reply({
+                embeds:
+                    [new Discord.MessageEmbed()
+                        .setTitle("Error")
+                        .setColor(utils.EmbedColors.Error)
+                        .setDescription('I was not able to fetch this user.')
+                        .setFooter({
+                            text: utils.Embeds.footerText,
+                            iconURL: discordClient.user.displayAvatarURL({dynamic: true})
+                        })
+                        .setTimestamp(new Date())]
+            })
+        }
 
+
+        let avatar = user.user.displayAvatarURL({dynamic: true});
+        if (!avatar.endsWith('?size=2048')) avatar += "?size=2048";
+
+        interaction.reply({
+            embeds:
+                [new Discord.MessageEmbed()
+                    .setImage(avatar)
+                    .setURL(avatar).setTitle("👤 Avatar")
+                    .setColor(utils.EmbedColors.Default)
+                    .setAuthor({
+                        url: user.user.displayAvatarURL({dynamic: true}),
+                        name: user.user.username
+                    })
+                    .setFooter({
+                        text: utils.Embeds.footerText,
+                        iconURL: discordClient.user.displayAvatarURL({dynamic: true})
+                    })
+                    .setTimestamp(new Date())]
+        })
 
     },
 };
