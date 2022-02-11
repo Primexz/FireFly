0c035da6d4cad51e7c1d625fdb0b7a26c3b59346
Primexz <johann@pepebot.info>
2022-02-11 23:13:13 +0100

    improve: code formatting

--- commands/music/music_manager.js
+++ commands/music/music_manager.js
@@ -36,7 +36,7 @@ module.exports = {
                 .setThumbnail(currentSong.thumbnail)
                 .addField(":eye: Views", `> ${currentSong.views ? utils.formatInt(currentSong.views) : "NaN"}`, true)
                 .addField("👍 Likes", `> ${currentSong.likes ? utils.formatInt(currentSong.likes) : "NaN"}`, true)
-                .addField("👎 Dislikes", `> ${currentSong.source == "youtube" ? utils.formatInt(await utils.getYtDislikes(currentSong.id) ) : "NaN"}`, true)
+                .addField("👎 Dislikes", `> ${currentSong.source == "youtube" ? utils.formatInt(await utils.getYtDislikes(currentSong.id)) : "NaN"}`, true)
                 .addField("Time", `> ${queue.formattedCurrentTime}`, true)
                 .addField("Duration", `> ${currentSong.formattedDuration}`, true)
                 .addField("Queue", `> ${queue.songs.length <= 1 ? "1 song" : `${queue.songs.length} songs`} - ${queue.formattedDuration}`, true)
@@ -100,7 +100,6 @@ module.exports = {
                             .setLabel("Refresh")
                             .setEmoji("🔃")
                             .setStyle("PRIMARY"),
-
                     )
 
 
